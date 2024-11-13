c18d18db2676bccffa050ea30c61764817dd0e6f
yashmith18 <yashmithj@gmail.com>
2024-11-13 17:06:34 -0600

    Add GitHub Actions deploy workflow

---
+++ build/precache-manifest.332cf224cc76360431ddc25cf155afba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8d0df85983b0798bca9c03b40ccdade",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4da560ec509f7747b204",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "caec4e116a4b1082b8c4",
+    "url": "/static/js/2.5c5cb5fc.chunk.js"
+  },
+  {
+    "revision": "ffbbb7b5be57fde30b46",
+    "url": "/static/js/3.09420c7d.chunk.js"
+  },
+  {
+    "revision": "4da560ec509f7747b204",
+    "url": "/static/js/main.f857ea4f.chunk.js"
+  },
+  {
+    "revision": "f2459104b10839a022c5",
+    "url": "/static/js/runtime~main.02412b67.js"
+  }
+]);
